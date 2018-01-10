933669c86ded811548e68bf850f53196d0d566c2
PawelWorwa <pawel.worwa@interia.pl>
2018-01-10 19:55:00 +0100

    * removed unused import

--- src/main/java/com/bdd/framework/driver/DriverFactory.java
+++ src/main/java/com/bdd/framework/driver/DriverFactory.java
@@ -1,75 +1,74 @@
 package com.bdd.framework.driver;
 
 import com.bdd.framework.browser.BrowserType;
-import com.bdd.framework.exceptions.UnsupportedBrowserException;
-import java.io.IOException;
-
-import org.openqa.selenium.WebDriver;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+
+import java.io.IOException;
 
 public class DriverFactory {
     private DriverFactory() {
         throw new IllegalStateException("DriverFactory is utility class and should not be instantiated!");
     }
 
-	private static final Logger LOGGER = LogManager.getLogger(DriverFactory.class);
+    private static final Logger LOGGER = LogManager.getLogger(DriverFactory.class);
 
-	private static WebDriver driver = null;
+    private static WebDriver driver = null;
 
-	public static WebDriver getDriver() {
-	    boolean seleniumGrid = Boolean.parseBoolean(
-	            System.getProperty("maven.use.selenium.grid"));
+    public static WebDriver getDriver() {
+        boolean seleniumGrid = Boolean.parseBoolean(
+                System.getProperty("maven.use.selenium.grid"));
 
-		if (driver == null) {
-		    if(seleniumGrid) {
-		        String eMessage = "Selenium grid is not supported at this moment.";
-		        LOGGER.error(eMessage);
-		        throw new IllegalStateException(eMessage);
+        if (driver == null) {
+            if (seleniumGrid) {
+                String eMessage = "Selenium grid is not supported at this moment.";
+                LOGGER.error(eMessage);
+                throw new IllegalStateException(eMessage);
 
             } else {
                 driver = LocalDriver.setup();
-                LOGGER.info("Started driver: "+ DriverConfiguration.BROWSER);
+                LOGGER.info("Started driver: " + DriverConfiguration.BROWSER);
             }
-		}
-		
-		return driver;
-	}
+        }
 
-	public static void resetDriver() throws IOException {
-		if(isDriverInitialised()) {
-			driver.quit();
-			driver = null;
-			processCleanup();
-		}
-	}
+        return driver;
+    }
+
+    public static void resetDriver() throws IOException {
+        if (isDriverInitialised()) {
+            driver.quit();
+            driver = null;
+            processCleanup();
+        }
+    }
 
     public static boolean isDriverInitialised() {
         return driver != null;
     }
 
-	public static void close() {
-		if (isDriverInitialised()) {
+    public static void close() {
+        if (isDriverInitialised()) {
             LOGGER.info("Shutting down driver: " + DriverConfiguration.BROWSER);
-			driver.quit();
-		}
-	}
+            driver.quit();
+        }
+    }
 
-	public static void processCleanup() throws IOException {
+    public static void processCleanup() throws IOException {
         BrowserType browser = BrowserType.valueOf(DriverConfiguration.BROWSER);
 
-        if(browser == BrowserType.INTERNET_EXPLORER) {
+        if (browser == BrowserType.INTERNET_EXPLORER) {
             killProcess("taskkill /F /IM IEDriverServer.exe");
 
-        } else if(browser == BrowserType.CHROME) {
+        } else if (browser == BrowserType.CHROME) {
             killProcess("taskkill /F /IM chromedriver.exe");
 
-        } else if(browser == BrowserType.FIREFOX) {
+        } else if (browser == BrowserType.FIREFOX) {
             killProcess("taskkill /F /IM geckodriver.exe");
         }
-	}
+    }
 
-	private static void killProcess(String command) throws IOException{
+    private static void killProcess(String command) throws IOException {
         Runtime.getRuntime().exec(command);
-	}
+    }
 }
